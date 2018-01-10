a0d5730ae5fe0567036327a0beb44db01e026fab
Roel Heesterbeek <roel.heesterbeek@gmail.com>
2018-01-10 10:30:21 +0100

    added flex-layout, material design, favicon, editor component and navigation drafts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 //import { NgIf } from '@angular/common';
-import { HeroService } from './hero.service';
 
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatCheckbox } from '@angular/material';
+
+import { HeroService } from './hero.service';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { EditorComponent } from './editor/editor.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    EditorComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    MatButtonModule,
+    MatCheckboxModule,
+    NoopAnimationsModule,
+    FormsModule,
+    FlexLayoutModule
   ],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
+import './polyfills'; // backwards compatibility
+import 'hammerjs'; // Gesture support for Material
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+
+/*
+// Gebruiken voor maps
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+*/
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
